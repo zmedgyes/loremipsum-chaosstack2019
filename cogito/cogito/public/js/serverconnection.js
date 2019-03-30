819f42ce3b4f5fde6ea0fe9d25ac3a0dbe28958f
Zsolt Medgyesi <zmedgyes@gmail.com>
2019-03-30 11:22:31 +0100

    client side server connection

---
+++ cogito/cogito/public/js/serverconnection.js
@@ -0,0 +1,19 @@
+﻿function serverRequest(url,body,callback) {
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4) {
+            var res;
+            var err;
+            try {
+                res = JSON.parse(xmlhttp.response);
+            }
+            catch (e) {
+                err = e;
+            }
+            callback(err, res);
+        }
+    };
+    xmlhttp.open("POST", url);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify(body));
+}
