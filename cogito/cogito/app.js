f338f29316928d63377d0690ffd31036ab226ae2
Zsolt Medgyesi <zmedgyes@users.noreply.github.com>
2019-03-30 10:58:26 +0100

    backend init

--- cogito/cogito/app.js
+++ cogito/cogito/app.js
@@ -1,3 +1,97 @@
-'use strict';
+const express = require('express');
+const async = require('async');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const app = express();
+const port = 3000;
 
-console.log('Hello world');
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(session({
+    secret: 'fdzsfdjgzghjg',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true }
+}));
+
+//DEBUG
+app.use((req, res, next) => {
+    if (req.body.studentid) {
+        req.session.studentid = req.body.studentid;
+    }
+    next();
+});
+
+app.get('/login', (req, res) => {
+    if (req.body.studentid) {
+
+        req.session.studentid = req.body.studentid;
+        res.json({ success: true });
+    }
+    else {
+        res.json({ success: false });
+    }
+});
+app.get('/logout', (req, res) => {
+    if (req.session.studentid) {
+        req.session.studentid = null;
+    }
+    res.json({success:true});
+});
+
+//req: { topic:(téma) }
+app.get('/getQuestion', (req, res) => {
+    if (req.session.studentid) {
+        let ret = {
+            success: true,
+            data: {
+                questionId: 1, // ezt majd vissza kell küldene
+                question: "Mia?",
+                answers: [
+                    'valasz1',
+                    'valasz2',
+                    'valasz3',
+                    'valasz4'
+                ]
+            }
+        };
+        res.json(ret);
+    }
+    else {
+        res.json({ success: false, data: "Unauthorized" });
+    }
+});
+
+//req { questionId: 1, answer:"valasz2"};
+app.post('/sendAnswer', (req, res) => {
+    if (req.session.studentid) {
+        let ret = {
+            success: true,
+            data: {
+                correct: true
+            }
+        };
+        res.json(ret);
+    }
+    else {
+        res.json({ success: false, data: "Unauthorized" });
+    }
+});
+
+app.get('/getScore', (req, res) => {
+    if (req.session.studentid) {
+        var ret = {
+            success: true,
+            data: {
+                //TODO
+            }
+        };
+        res.json(ret);
+    }
+    else {
+        res.json({ success: false, data: "Unauthorized" });
+    }
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ cogito/cogito/database.js
@@ -0,0 +1,18 @@
+﻿class Database {
+    constructor() {
+    }
+    connect(callback) {
+    }
+    close(callback) {
+    }
+    saveUser(studentid, callback) {
+    }
+    getQuestion(params, callback) {
+    }
+    saveAnswer(params, callback) {
+    }
+    getScore(params, callback) {
+    }
+};
+
+module.exports = Database
